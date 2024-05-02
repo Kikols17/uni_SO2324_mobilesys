dcf1fde2e9712c91bdf188a82f13c9b6c013b1ba
Kikols <francisco.lapamsilva@gmail.com>
2024-05-02 17:18:05 +0100

    backoffice can now make both "data_stats" and "reset" requests, at user's will

--- src/backoffice_user.c
+++ src/backoffice_user.c
@@ -17,6 +17,12 @@
 #define BACKEND_PIPE "/tmp/backend_pipe"
 
 
+int auth5g_request(char *req_type);
+
+
+int backendpipe_fd;
+
+
 int main(int argc, char *argv[]) {
     char buff_out[BUF_SIZE];            // Store messages to write to pipe
 
@@ -25,14 +31,43 @@ int main(int argc, char *argv[]) {
     }
 
 
-    int backendpipe_fd;
     if ((backendpipe_fd = open(BACKEND_PIPE, O_WRONLY)) < 0) {
-        perror("[ERROR]: Cannot open pipe for writing");
+        fprintf(stderr, "[ERROR]: Cannot open pipe for writing");
         exit(0);
     }
+
+    char option[2];
+    while (1) {
+        fprintf(stdout, "\n0: data_stats - apresenta estatísticas referentes aos consumos dos dados nos vários serviços:\n"
+        "total de dados reservados e número de pedidos de renovação de autorização;\n"
+        "1: reset - limpa as estatísticas relacionadas calculadas até ao momento pelo sistema.\n");
+        fgets(option, 2, stdin);
+        if (option[0]=='0') {
+            auth5g_request("data_stats");
+        } else if (option[0]=='1') {
+            auth5g_request("reset");
+        } else {
+            fprintf(stderr, "[ERROR]: Invalid option\n");
+        }
+    } 
 
     sprintf(buff_out, "%d#%s", getpid(), "data_stats");
     printf("MAKE REQUEST: \"%s\"\n", buff_out);
     write(backendpipe_fd, &buff_out, sizeof(buff_out));
+
+    return 1;
+}
+
+
+int auth5g_request(char *req_type) {
+    char buff_out[BUF_SIZE];            // Store messages to write to pipe
+
+    sprintf(buff_out, "%d#%s", getpid(), req_type);
+    printf("MAKE REQUEST: \"%s\"\n", buff_out);
+    if (write(backendpipe_fd, &buff_out, sizeof(buff_out))==0 ) {
+        fprintf(stderr, "[ERROR]: Cannot write to pipe");
+        return 0;
+    }
+
     return 1;
 }
