79665755c9f47fad3ffbcb0f4799f0c2fd9a64ef
migsonic08 <130188603+migsonic08@users.noreply.github.com>
2024-05-10 17:21:18 +0100

    Update teste.sh

--- src/teste.sh
+++ src/teste.sh
@@ -2,6 +2,7 @@
 
 
 do
+    gnome-terminal -- bash -c "./5g_auth_plataform.c 5gconfig.config; exec bash"
     gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
     gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
     gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
