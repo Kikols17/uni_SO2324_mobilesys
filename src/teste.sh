bc9ac4ef071a51b124cec4230a3445a456b717fd
migsonic08 <130188603+migsonic08@users.noreply.github.com>
2024-05-10 17:20:24 +0100

    olá

---
+++ src/teste.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+do
+    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
+done
