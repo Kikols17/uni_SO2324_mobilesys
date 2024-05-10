22cf33e8580c43fb6037314f082296f209c26cbd
migsonic08 <130188603+migsonic08@users.noreply.github.com>
2024-05-10 17:32:05 +0100

    Update teste.sh

--- src/teste.sh
+++ src/teste.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-    gnome-terminal -- bash -c "./5g_auth_plataform.c 5gconfig.config; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 30 1000 2000 5000; exec bash"
+    gnome-terminal -- bash -c "./5g_auth_platform 5gconfig.config; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
