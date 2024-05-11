6ce61238a1262c5f6ddb6967b94232779abeb7ca
Kikols <francisco.lapamsilva@gmail.com>
2024-05-11 01:53:19 +0100

    make mobile_spawner.sh exit on end

--- src/mobile_spawner.sh
+++ src/mobile_spawner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
     #gnome-terminal -- bash -c "./5g_auth_platform 5gconfig.config; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
-    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash & exit"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash & exit"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash & exit"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash & exit"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash & exit"
+    gnome-terminal -- bash -c "./mobile_user 1000 50 1000 2000 5000 50; exec bash & exit"
